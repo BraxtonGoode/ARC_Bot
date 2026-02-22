c1f228a23776c5f7ab65ba2302f167dfb7493cbc
BraxtonGoode <tjbgoode@gmail.com>
2026-02-22 02:06:04 -0700

    fixed name location

--- src/utils/invasionEvents.js
+++ src/utils/invasionEvents.js
@@ -105,7 +105,7 @@ class InvasionEvents {
         privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
         entityType: GuildScheduledEventEntityType.External,
         entityMetadata: {
-          location: 'Avatar: Generations Game',
+          location: 'Avatar Realms Collide',
         },
       });
 
